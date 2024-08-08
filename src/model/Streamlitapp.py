bbe988a126fc79b4dcdc5822eb54d36ec9b11d73
katia0610 <120342025+katia0610@users.noreply.github.com>
2024-08-08 15:04:22 +0200

    amelioration interface

--- src/model/Streamlitapp.py
+++ src/model/Streamlitapp.py
@@ -9,6 +9,7 @@ embedding_function = load_embedding_function()
 GROQ_TOKEN = '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'
 llm = ChatGroq(model_name='llama-3.1-70b-versatile', api_key=GROQ_TOKEN, temperature=0)
 url="https://a08399e1-9b23-417d-bc6a-88caa066bca4.us-east4-0.gcp.cloud.qdrant.io:6333"
+
 api_key= 'lJo8SY8JQy7W0KftZqO3nw11gYCWIaJ0mmjcjQ9nFhzFiVamf3k6XA'
 collection_name= "lvHP_collection"
 # Initialiser le vectorstore et le retriever
@@ -20,14 +21,26 @@ st.set_page_config(
     page_title="EquotIA",
     page_icon="🧠",
 )
-st.title(" 🧠 Sales Smart Assistant DGF")
+st.title("🧠 Sales Smart Assistant DGF")
+
+# Initialiser la session_state pour stocker l'historique des messages
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+# Entrée utilisateur
 question = st.chat_input("ex : trouve les Ordinateurs intel core i5 de la marque Samsung")
 
 if question:
+    # Append the user's input to the chat history
+    st.session_state.messages.append({"role": "user", "content": question})
+    
+    # Get the bot's response
     result = query_bot(retriever, embedding_function, question)
     
-    # Affichage du résultat de la question
-    st.markdown("### Résultat")
-    st.markdown(result)
-
+    # Append the bot's response to the chat history
+    st.session_state.messages.append({"role": "bot", "content": result})
     
+    # Display the conversation
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
