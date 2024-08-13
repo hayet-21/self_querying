e1983971d89faeb475dbf60e955e21f4cbade92d
hayet touati <82230012+hayet-21@users.noreply.github.com>
2024-08-13 14:26:12 +0100

    exec time

--- src/model/Streamlitapp.py
+++ src/model/Streamlitapp.py
@@ -1,6 +1,9 @@
 import streamlit as st
-from pipline import load_embedding_function, initialize_vectorstore, initialize_retriever, query_bot ,extract_product_info
+from pipline import load_embedding_function, initialize_vectorstore, initialize_retriever, query_bot
 from langchain_groq import ChatGroq
+import asyncio
+import time
+from langchain_core.prompts import ChatPromptTemplate
 
 # Charger la fonction d'embedding
 embedding_function = load_embedding_function()
@@ -12,10 +15,50 @@ url="https://a08399e1-9b23-417d-bc6a-88caa066bca4.us-east4-0.gcp.cloud.qdrant.io
 
 api_key= 'lJo8SY8JQy7W0KftZqO3nw11gYCWIaJ0mmjcjQ9nFhzFiVamf3k6XA'
 collection_name="lenovoHP_collection"
+
+document_content_description = "Informations sur le produit, incluant la référence et la description."
+metadata_field_info = [
+        {
+            'name': "Marque",
+            'description': "La Marque du produit.",
+            'type': "string",
+        },
+        {
+            'name': "Categorie",
+            'description': "La Categorie du produit.",
+            'type': "string",
+        }
+    ]
 # Initialiser le vectorstore et le retriever
 vectorstore = initialize_vectorstore(embedding_function,url,api_key,collection_name)
-retriever = initialize_retriever(llm, vectorstore)
+retriever = initialize_retriever(llm, vectorstore,metadata_field_info,document_content_description)
+# Construire le template de prompt
+prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                'system',
+                """
+                Tu es un assistant vendeur. Tu as accès au contexte seulement. Ne génère pas des informations si elles ne sont pas dans le contexte. 
+                Répond seulement si tu as la réponse. Affiche les produits un par un sous forme de tableau qui contient ces colonne Référence,Categorie, Marque, Description.
+                Il faut savoir que laptop, ordinateur, ordinateurs portable , pc et poste de travail ont tous le même sens.
+                Il faut savoir que téléphone portable et smartphone ont le même sens.
+                Il faut savoir que tout autre caractéristique du produit tel que la RAM stockage font partie de la description du produit et il faut filtrer selon la marque et la catégorie seulement.
+                Si le contexte est vide, dis-moi que tu n'as pas trouvé de produits correspondants. Je veux que la réponse soit claire et facile à lire, avec des sauts de ligne pour séparer chaque produit. Ne me donne pas de produits qui ne sont pas dans le contexte.
+                lorsque une question de similarite entre des produits est poser, il faut dabord commencer par les produit qui ont des processeur qui se ressemble le plus, puis la memoire ram , puis le stockage, puis les autres caracteristique
+                la question peut contenir  plusieurs produits avec differentes descriptions, il faut chercher sur les differents produits demandé .
+                si je te pose une question sur les question ou les reponses fournient precedement tu doit me repondre selon l'historique.
+                tu ne doit pas oublier l'historique car parfois le user continue a te poser des question sur tes reponses que tas deja fourni aupatavant
 
+                Contexte: {context}
+                historique :{historique}
+                Question: {question}
+
+                Réponse :
+                """
+            ),
+        ]
+    )
+    
 # Interface Streamlit
 st.set_page_config(
     page_title="EquotIA",
@@ -33,12 +76,13 @@ question = st.chat_input("ex : trouve les Ordinateurs intel core i5 de la marque
 if question:
     # Append the user's input to the chat history
     st.session_state.messages.append({"role": "user", "content": question})
-    
+    start_time =time.time()
     # Get the bot's response
-    result = query_bot(retriever, embedding_function, question)
-    
+    result= asyncio.run(query_bot(retriever, embedding_function, question,prompt))
+    #print(f"Résultat: {result}, Temps d'exécution: {exec_time} secondes")
+    exec_time=time.time() - start_time
     # Append the bot's response to the chat history
-    st.session_state.messages.append({"role": "ai", "content": result})
+    st.session_state.messages.append({"role": "ai", "content" :f"{result}\n\n(Temps d'exécution: {exec_time:.2f} secondes)"})
     
     # Display the conversation
     for message in st.session_state.messages:
--- src/model/pipline.py
+++ src/model/pipline.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 import os
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.vectorstores import Chroma
-from langchain_core.prompts import ChatPromptTemplate
 from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_groq import ChatGroq
@@ -11,6 +10,8 @@ from langchain.memory import ConversationBufferMemory # Import de la mémoire
 from langchain_community.vectorstores import Qdrant
 import qdrant_client
 
+
+#trouve les PC HP intel core i7
 # Charger les variables d'environnement
 load_dotenv()
 
@@ -20,10 +21,9 @@ CHROMA_PATH = os.path.abspath(f"../{os.getenv('CHROMA_PATH')}")
 COLLECTION_CSV = os.getenv('COLLECTION_CSV')
 GROQ_TOKEN = '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'
 llm = ChatGroq(model_name='llama-3.1-70b-versatile', api_key=GROQ_TOKEN, temperature=0)
-
+#llama3-8b-8192
 # Initialize memory and conversation chain globally
 memory = ConversationBufferMemory()
-
 def load_embedding_function():
     try:
         embedding_function = HuggingFaceInferenceAPIEmbeddings(
@@ -43,21 +43,7 @@ def initialize_vectorstore(embedding_function, QDRANT_URL, QDRANT_API_KEY, colle
     return Qdrant(qdrantClient, collection_name, embedding_function) #, vector_name='vector_params'
 
 
-def initialize_retriever(llm, vectorstore):
-    document_content_description = "Informations sur le produit, incluant la référence et la description."
-    metadata_field_info = [
-        {
-            'name': "Marque",
-            'description': "La Marque du produit.",
-            'type': "string",
-        },
-        {
-            'name': "Categorie",
-            'description': "La Categorie du produit.",
-            'type': "string",
-        }
-    ]
-
+def initialize_retriever(llm, vectorstore,metadata_field_info,document_content_description):
     retriever = SelfQueryRetriever.from_llm(
         llm,
         vectorstore,
@@ -68,7 +54,7 @@ def initialize_retriever(llm, vectorstore):
     )
     return retriever
 
-def query_bot(retriever, embedding_function, question):
+async def query_bot(retriever, embedding_function, question,prompt):
     context = retriever.invoke(question)
     if not context:
         return "Je n'ai pas trouvé de produits correspondants."
@@ -80,39 +66,10 @@ def query_bot(retriever, embedding_function, question):
     filtered_docs = [
         doc for doc, similarity in zip(context, similarities) if similarity >= 0.7
     ]
-    
-    # Construire le template de prompt
-    prompt = ChatPromptTemplate.from_messages(
-            [
-                (
-                    'system',
-                    """
-                    Tu es un assistant vendeur. Tu as accès au contexte seulement. Ne génère pas des informations si elles ne sont pas dans le contexte. 
-                    Répond seulement si tu as la réponse. Affiche les produits un par un sous forme de tableau qui contient ces colonne Référence,Categorie, Marque, Description.
-                    Il faut savoir que laptop, ordinateur, ordinateurs portable , pc et poste de travail ont tous le même sens.
-                    Il faut savoir que téléphone portable et smartphone ont le même sens.
-                    Il faut savoir que tout autre caractéristique du produit tel que la RAM stockage font partie de la description du produit et il faut filtrer selon la marque et la catégorie seulement.
-                    Si le contexte est vide, dis-moi que tu n'as pas trouvé de produits correspondants. Je veux que la réponse soit claire et facile à lire, avec des sauts de ligne pour séparer chaque produit. Ne me donne pas de produits qui ne sont pas dans le contexte.
-                    lorsque une question de similarite entre des produits est poser, il faut dabord commencer par les produit qui ont des processeur qui se ressemble le plus, puis la memoire ram , puis le stockage, puis les autres caracteristique
-
-                    si je te pose une question sur les question ou les reponses fournient precedement tu doit me repondre selon l'historique.
-                    tu ne doit pas oublier l'historique car parfois le user continue a te poser des question sur tes reponses que tas deja fourni aupatavant
-    
-                    Contexte: {context}
-                    historique :{historique}
-                    Question: {question}
-
-                    Réponse :
-                    """
-                ),
-            ]
-        )
-    
     document_chain = create_stuff_documents_chain(llm, prompt)
               
     # Charger l'historique des conversations
     conversation_history = memory.load_memory_variables({})
-
     result = document_chain.invoke(
             {
                 "context": filtered_docs,
@@ -120,28 +77,8 @@ def query_bot(retriever, embedding_function, question):
                 "question": question  # Utiliser 'question' au lieu de 'messages'
             },
     )
-
     # Save context
     memory.save_context({"question": question}, {"response": result})
+    
 
     return result
-
-def extract_product_info(text):
-    if not text or text.strip() == "Je n'ai pas trouvé de produits correspondants.":
-        return []
-    products = []
-    lines = text.strip().split('\n\n')
-    for line in lines:
-        product = {}
-        lines = line.split('\n')
-        for l in lines:
-            if l.startswith('- Référence: '):
-                product['Référence'] = l.replace('- Référence: ', '').strip()
-            elif l.startswith('- Categorie: '):
-                product['Categorie'] = l.replace('- Categorie: ', '').strip()
-            elif l.startswith('- Marque: '):
-                product['Marque'] = l.replace('- Marque: ', '').strip()
-            elif l.startswith('- Description: '):
-                product['Description'] = l.replace('- Description: ', '').strip()
-        products.append(product)
-    return products
